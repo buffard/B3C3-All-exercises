5dfa7d36d28d789e6c0535c86f2b4ac938e37f47
buffard <samuelwebber19@gmail.com>
2018-10-15 14:10:16 -0500

    updated with first three completed exercises

--- main.js
+++ main.js
@@ -1,19 +1,92 @@
+// *PRACTICE: CHICKEN MONKEY*
 // Write a program that prints the numbers from 1 to 100. But for multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number and for the multiples of seven (7, 14, 21, etc.) print "Monkey". For numbers which are multiples of both five and seven print "ChickenMonkey".
 // To determine if a number can be evenly divided by 5 or 7, use the JavaScript remainder operator.
 // You can start with this simpler version that outputs even numbers to the console and ignores odd numbers. You need to refactor this code to meet the requirements above.
 
-for (var num = 1; num <= 100; num++) {
+for (let num = 1; num <= 100; num++) {
   if (num % 7 === 0 && num % 5 === 0){
-    var i = "chickenMonkey"
-    console.log(i)
+    console.log('chickenMonkey')
   } else if (num % 5 === 0) {
-    var i = "chicken"
-    console.log(i)
+    console.log('chicken')
    } else if (num % 7 === 0) {
-     var i = "monkey"
-     console.log(num)
+     console.log('monkey')
   } else {
     console.log(num)
  }
 }
 
+// ===============================
+// *PRACTICE: TAKE A NUMBER -  BATTLE OF THE BANDS*
+// A local nightclub is having a Battle of the Bands night in a few months. They expect many bands to sign up because the grand prize is $20,000. To make things easier on the nightclub management, they want each band to be assigned a number so that they can easily keep track of the order in which the bands will perform.
+
+// Your job is to write a function that accepts any band name as an argument. The function will increment a global variable by one each time it is invoked, and return that number, and the band name concatenated together.
+
+let bandNumber = 0
+
+const takeNumber = function (bandName) {
+  bandNumber++;
+  return `${bandNumber}. ${bandName}`
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
+
+// ===================================
+
+// *PRACTICE: COOKOUT*
+// Copy the following objects into your JavaScript file.
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An first array containing the objects to be cooked.
+const uncookedFood = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+// This exercise does not require you to alter this function. But read it. See if you can follow what it takes in, executes, and returns.
+
+// The grill() function. Copy this into your JavaScript as well.
+  
+function grill (currentObject) {
+  // Modify the food so that it is cooked
+  currentObject.cooked = true;
+
+  // Put the cooked food into the appropriate array
+  cookedFood.push(currentObject);
+};
+
+// Your task is to iterate over the array of uncookedFood and invoke the function for each item so that the cookedFood array contains all of the items after they are cooked.
+
+
+//below is the code I wrote
+
+for (let i = 0; i < uncookedFood.length; i++) {
+  grill(uncookedFood[i]);
+}
